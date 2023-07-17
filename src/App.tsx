aef0cb6e07de5428ea16c09138256e27ec127c4d
Miguel <migueljomartinez@gmail.com>
2023-07-16 23:21:05 -0500

    Using hood for state machine

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useMachine } from "@xstate/react";
+
+import { BaseLayout } from "./components/BaseLayout";
+import { bookingMachine } from "./machines/booking-machine";
 
 function App() {
+  const [state, send] = useMachine(bookingMachine, { devTools: true });
+
+  console.log("State: ", state);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BaseLayout />
     </div>
   );
 }
